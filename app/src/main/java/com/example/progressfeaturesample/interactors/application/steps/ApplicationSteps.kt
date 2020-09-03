404d39fd6375004eae31528174a820a6848b5921
sokolova <sokolova@surfstudio.co>
2020-09-03 12:37:36 +0300

    добавлены комментарии

--- app/src/main/java/com/example/progressfeaturesample/interactors/application/steps/ApplicationSteps.kt
+++ app/src/main/java/com/example/progressfeaturesample/interactors/application/steps/ApplicationSteps.kt
@@ -2,9 +2,17 @@ package com.example.progressfeaturesample.interactors.application.steps
 
 import com.example.progressfeaturesample.interactors.common.step.Step
 
+/**
+ * Класс для ограничения иерархии классов с шагами
+ */
 sealed class ApplicationSteps : Step
+// Шаг с персональными данными
 object PersonalInfoStep : ApplicationSteps()
+// Шаг с инф-цией об образовании
 object EducationStep : ApplicationSteps()
+// Шаг с инф-цией об опыте работы
 object ExperienceStep : ApplicationSteps()
+// Шаг для написания эссе "о себе"
 object AboutMeStep : ApplicationSteps()
+// Шаг для выбора, что интересно в данной вакансии
 object MotivationStep : ApplicationSteps()
